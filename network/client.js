ab1f6f81d37d060eb64c27f8253fed90021b47f4
Hans Löfgren <hans.lofgren@jayway.com>
2019-03-07 10:49:16 +0100

    network-client: Exposed empty function interfaces for sending input and receiving state

---
+++ network/client.js
@@ -0,0 +1,15 @@
+// This file will expose one function for sending input to the server and one function used to bind the specific "receiveState" callback function,
+// which should execute on "stateChange" event sent from the server.
+
+function sendInput(input) {
+
+}
+
+function stateReceiver(callback) {
+
+}
+
+module.exports = {
+  sendInput,
+  stateReceiver
+};
