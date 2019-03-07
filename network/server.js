d3af4cc7ac4be4b91f45a04f2ee7d66cd47b83dd
Hans Löfgren <hans.lofgren@jayway.com>
2019-03-07 10:43:21 +0100

    network-server: Empty function interfaces exposed

---
+++ network/server.js
@@ -0,0 +1,17 @@
+/**
+ * This file will expose one function for broadcasting the latest state to all connected clients,
+ * it will also expose one function for binding the callback to use for receiving input.
+ */
+
+function broadcastState(state) {
+
+}
+
+function inputReceiver(callback) {
+
+}
+
+module.exports = {
+  broadcastState,
+  inputReceiver
+};
