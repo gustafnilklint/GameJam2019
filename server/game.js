360fe68f307414c9f7e3ecef4e55c359e16545c1
Gustaf Nilklint <gustaf.nilklint@jayway.com>
2019-03-15 12:23:01 +0100

    Made some changes to game backend

--- server/game.js
+++ server/game.js
@@ -2,24 +2,32 @@ const Matter = require("matter-js");
 
 const players = new Map();
 const world = {
-  apples: [],
-  bullets: []
+  apples: []
 };
 
 const SPEED = 5;
-const ROTATION_SPEED = 0.08;
-const BULLET_SPEED = 10;
+const ROTATION_SPEED = 0.5;
 
 /**
  * Adds a new player
  * @param {string} playerId
  */
+
+function tailPathFrom({ x, y }, length) {
+  const tail = [];
+  for (let i = 0; i < length; i + 1) {
+    tail.push({ x: x + 10 * i, y });
+  }
+  return tail;
+}
+
 function newPlayer(playerId) {
+  const spanwPosition = { x: Math.random() * 500, y: Math.random() * 500 };
   const initialState = {
-    x: Math.random() * 500,
-    y: Math.random() * 500,
-    width: 52,
-    height: 70,
+    x: spanwPosition.x,
+    y: spanwPosition.y,
+    length: 10,
+    path: tailPathFrom(spanwPosition, 10),
     rotation: Math.random() * 2 * Math.PI
   };
   players.set(playerId, {
@@ -44,32 +52,32 @@ function isAlive(id) {
   return players.has(id);
 }
 
-/**
- * Moves the bullets, and removes them if they are out of bounds
- * @param {{x: number, y: number, direction: Matter.Vector}[]} bullets The current bullets
- * @returns {{x: number, y: number, direction: Matter.Vector}[]} The new bullets
- */
-function moveBullets(bullets) {
-  return bullets
-    .filter(bullet => bullet)
-    .map(bullet => {
-      let newPositionVector = Matter.Vector.create(bullet.x, bullet.y);
-      newPositionVector = Matter.Vector.add(
-        newPositionVector,
-        Matter.Vector.mult(bullet.direction, BULLET_SPEED)
-      );
+// /**
+//  * Moves the bullets, and removes them if they are out of bounds
+//  * @param {{x: number, y: number, direction: Matter.Vector}[]} bullets The current bullets
+//  * @returns {{x: number, y: number, direction: Matter.Vector}[]} The new bullets
+//  */
+// function moveBullets(bullets) {
+//   return bullets
+//     .filter(bullet => bullet)
+//     .map(bullet => {
+//       let newPositionVector = Matter.Vector.create(bullet.x, bullet.y);
+//       newPositionVector = Matter.Vector.add(
+//         newPositionVector,
+//         Matter.Vector.mult(bullet.direction, BULLET_SPEED)
+//       );
 
-      // If bullet is out of bounds, remove from state
-      const WIDTH = 600;
-      const HEIGHT = 600;
-      const { x, y } = newPositionVector;
-      const bulletIsOutOfBounds = x > WIDTH || x < 0 || y > HEIGHT || y < 0;
-      return bulletIsOutOfBounds
-        ? null
-        : { ...bullet, x: newPositionVector.x, y: newPositionVector.y };
-    })
-    .filter(b => !!b);
-}
+//       // If bullet is out of bounds, remove from state
+//       const WIDTH = 600;
+//       const HEIGHT = 600;
+//       const { x, y } = newPositionVector;
+//       const bulletIsOutOfBounds = x > WIDTH || x < 0 || y > HEIGHT || y < 0;
+//       return bulletIsOutOfBounds
+//         ? null
+//         : { ...bullet, x: newPositionVector.x, y: newPositionVector.y };
+//     })
+//     .filter(b => !!b);
+// }
 
 /**
  * Turns the player if the `a` or `d` key has been pressed
@@ -90,40 +98,16 @@ function turnPlayer(pressedKeys, state) {
  * @returns {{x: number, y: number}} The new position of the player
  */
 function movePlayer(pressedKeys, state) {
-  if (pressedKeys.w === "keydown" || pressedKeys.s === "keydown") {
-    const speed = pressedKeys.w === "keydown" ? SPEED : -SPEED;
+  const speed = SPEED;
 
-    let translation = Matter.Vector.create(0, -1);
-    translation = Matter.Vector.rotate(translation, state.rotation);
-    translation = Matter.Vector.mult(translation, speed);
+  let translation = Matter.Vector.create(0, -1);
+  translation = Matter.Vector.rotate(translation, state.rotation);
+  translation = Matter.Vector.mult(translation, speed);
 
-    const currentPosition = Matter.Vector.create(state.x, state.y);
-    const nextPosition = Matter.Vector.add(currentPosition, translation);
+  const currentPosition = Matter.Vector.create(state.x, state.y);
+  const nextPosition = Matter.Vector.add(currentPosition, translation);
 
-    return { x: nextPosition.x, y: nextPosition.y };
-  }
-  return { x: state.x, y: state.y };
-}
-
-/**
- * Shoots if space key was released.
- * @returns {void}
- */
-function shoot(pressedKeys, state) {
-  if (pressedKeys.space === "release") {
-    const direction = Matter.Vector.rotate(
-      Matter.Vector.create(0, -1),
-      state.rotation
-    );
-
-    world.bullets.push({
-      x: state.x + direction.x * state.height,
-      y: state.y + direction.y * state.width,
-      radius: 6,
-      direction
-    });
-    pressedKeys.space = "keyup";
-  }
+  return { x: nextPosition.x, y: nextPosition.y };
 }
 
 /**
@@ -133,35 +117,32 @@ function shoot(pressedKeys, state) {
  */
 function updatePlayer(player, id) {
   const { pressedKeys, state } = player;
+  if (pressedKeys.a === "release") {
+    state.rotation = turnPlayer(pressedKeys, state);
 
-  state.rotation = turnPlayer(pressedKeys, state);
+    const { x, y } = movePlayer(pressedKeys, state);
+    state.path = [{ x, y }, { x: x + 10, y: y + 10 }];
+    state.x = x;
+    state.y = y;
 
-  const { x, y } = movePlayer(pressedKeys, state);
-  state.path = [{ x, y }, { x: x + 5, y }, { x: x + 10, y }, { x: x + 15, y }];
-  state.x = x;
-  state.y = y;
-
-  shoot(pressedKeys, state);
-
-  players.set(id, { ...player, state });
+    players.set(id, { ...player, state });
+  }
 }
 
 function checkCollisions() {
   // Check all players vs all bullets for spherical collision
-  const { bullets } = world;
-  bullets.forEach(bullet => {
-    players.forEach(({ state: player }, playerId) => {
-      const playerRadius = Math.min(player.width, player.height);
-
-      const dx = player.x - bullet.x;
-      const dy = player.y - bullet.y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < playerRadius + bullet.radius) {
-        players.delete(playerId);
-      }
-    });
-  });
+  // const { bullets } = world;
+  // bullets.forEach(bullet => {
+  //   players.forEach(({ state: player }, playerId) => {
+  //     const playerRadius = Math.min(player.width, player.height);
+  //     const dx = player.x - bullet.x;
+  //     const dy = player.y - bullet.y;
+  //     const distance = Math.sqrt(dx * dx + dy * dy);
+  //     if (distance < playerRadius + bullet.radius) {
+  //       players.delete(playerId);
+  //     }
+  //   });
+  // });
 }
 
 /**
@@ -171,8 +152,7 @@ function checkCollisions() {
 function updateState() {
   players.forEach(updatePlayer);
 
-  world.bullets = moveBullets(world.bullets);
-  world.apples = [{ x: 100, y: 100 }, { x: 200, y: 200 }];
+  // world.bullets = moveBullets(world.bullets);
 
   checkCollisions();
 
